8a6f1df847947d2e2f661233e723be1c7804af62
evanng07 <ngoeva072@gmail.com>
2023-05-09 04:49:04 -0500

    added players

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -9,24 +9,60 @@ const TeamPage = () => {
           Your Team
         </div>
         <div className="team">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
+            <Card>
+                Patrick Mahomes
+            </Card>
+            <Card>
+                Christian McCaffrey
+            </Card>
+            <Card>
+                D'eandre Swift
+            </Card>
+            <Card>
+                D.J. Moore
+            </Card>
+            <Card>
+                Jaylen Waddle
+            </Card>
+            <Card>
+                Mark Andrews
+            </Card>
+            <Card>
+                Ceedee Lamb
+            </Card>
+            <Card>
+                Juju Smith-Schuster
+            </Card>
+            <Card>
+                Matt Prater
+            </Card>
+            <Card>
+                Adam Thielen
+            </Card>
+            <Card>
+                AJ Dillon
+            </Card>
+            <Card>
+                Rhamondre Stevenson
+            </Card>
+            <Card>
+                Rashad Penny
+            </Card>
+            <Card>
+                Kenny Golladay
+            </Card>
+            <Card>
+                Michael Gallup
+            </Card>
+            <Card>
+                Joe Mixon
+            </Card>
+            <Card>
+                Travis Kelce
+            </Card>
+            <Card>
+                Matt Gay
+            </Card>
         </div>
       </div>
     
