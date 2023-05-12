69d95d9e5ccb01c8c722747d7779f46639389906
evanng07 <ngoeva072@gmail.com>
2023-05-12 04:13:39 -0500

    changed some stuff

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -8,126 +8,126 @@ const TeamPage = () => {
       <div className="team">
         <Card>
           <div>Patrick Mahomes</div>
-          <div className="text-center mt-2">QB - 15</div>
+          <div className="text-center mt-2">QB | 15</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Kansas City Chiefs
           </div>
         </Card>
         <Card>
           <div>Christian McCaffrey</div>
-          <div className="text-center mt-2">RB - 23</div>
+          <div className="text-center mt-2">RB | 23</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             San Francisco 49ers
           </div>
         </Card>
         <Card>
           <div>D'eandre Swift</div>
-          <div className="text-center mt-2">RB - 0</div>
+          <div className="text-center mt-2">RB | 0</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Philadelphia Eagles
           </div>
         </Card>
         <Card>
           <div>D.J. Moore</div>
-          <div className="text-center mt-2">WR - 2</div>
+          <div className="text-center mt-2">WR | 2</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Carolina Panthers
           </div>
         </Card>
         <Card>
           <div>Jaylen Waddle</div>
-          <div className="text-center mt-2">WR - 17</div>
+          <div className="text-center mt-2">WR | 17</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Miami Dolphins
           </div>
         </Card>
         <Card>
           <div>Mark Andrews</div>
-          <div className="text-center mt-2">TE - 89</div>
+          <div className="text-center mt-2">TE | 89</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Baltimore Ravens
           </div>
         </Card>
         <Card>
           <div>Ceedee Lamb</div>
-          <div className="text-center mt-2">WR - 88</div>
+          <div className="text-center mt-2">WR | 88</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Dallas Cowboys
           </div>
         </Card>
         <Card>
           <div>Juju Smith-Schuster</div>
-          <div className="text-center mt-2">WR - 9</div>
+          <div className="text-center mt-2">WR | 9</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             New England Patriots
           </div>
         </Card>
         <Card>
           <div>Matt Prater</div>
-          <div className="text-center mt-2">K - 5</div>
+          <div className="text-center mt-2">K | 5</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Arizona Cardinals
           </div>
         </Card>
         <Card>
           <div>Adam Thielen</div>
-          <div className="text-center mt-2">WR - 19</div>
+          <div className="text-center mt-2">WR | 19</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Carolina Panthers
           </div>
         </Card>
         <Card>
           <div>AJ Dillon</div>
-          <div className="text-center mt-2">RB - 28</div>
+          <div className="text-center mt-2">RB | 28</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Green Bay Packers
           </div>
         </Card>
         <Card>
           <div>Rhamondre Stevenson</div>
-          <div className="text-center mt-2">RB - 38</div>
+          <div className="text-center mt-2">RB | 38</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
           New England Patriots
           </div>
         </Card>
         <Card>
           <div>Rashad Penny</div>
-          <div className="text-center mt-2">RB - 23</div>
+          <div className="text-center mt-2">RB | 23</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Philadelphia Eagles
           </div>
         </Card>
         <Card>
           <div>Kenny Golladay</div>
-          <div className="text-center mt-2">WR - 19</div>
+          <div className="text-center mt-2">WR | 19</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             New York Giants
           </div>
         </Card>
         <Card>
           <div>Michael Gallup</div>
-          <div className="text-center mt-2">WR - 13</div>
+          <div className="text-center mt-2">WR | 13</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
           Dallas Cowboys
           </div>
         </Card>
         <Card>
           <div>Joe Mixon</div>
-          <div className="text-center mt-2">RB - 28</div>
+          <div className="text-center mt-2">RB | 28</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Cincinnati Bengals
           </div>
         </Card>
         <Card>
           <div>Travis Kelce</div>
-          <div className="text-center mt-2">TE - 87</div>
+          <div className="text-center mt-2">TE | 87</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Kansas City Chiefs
           </div>
         </Card>
         <Card>
           <div>Matt Gay</div>
-          <div className="text-center mt-2">K - 8</div>
+          <div className="text-center mt-2">K | 8</div>
           <div className="text-center bottom-6 absolute text-xs left-0 right-0">
             Indianapolis Colts
           </div>
