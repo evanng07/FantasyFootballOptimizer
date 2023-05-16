592523fcf9d7f0c4eba9849e00b9b94df8d3ef88
IbrahimKhanGH <iboo11khan@gmail.com>
2023-05-16 02:40:28 -0500

    shoutout evan

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import { Route, Routes, Navigate, Link } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import HomePage from './pages/HomePage';
-import TeamPage from './pages/TeamPage';
-import SearchPage from './pages/SearchPage';
-import StatPage from './pages/StatPage';
-
+import logo from "./logo.svg";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import TeamPage from "./pages/TeamPage";
+import SearchPage from "./pages/SearchPage";
+import StatPage from "./pages/StatPage";
 
 function App() {
   return (
-    
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/team' element={<TeamPage />} />
-          <Route path='/stat' element={<StatPage />} />
-          <Route path='/search' element={<SearchPage />} />
-        </Route>
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index={true} element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/stat" element={<StatPage />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Route>
+    </Routes>
   );
 }
 
