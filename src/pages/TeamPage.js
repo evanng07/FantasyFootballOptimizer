b18bc03e2e55b348e16e6d418788c624144d5852
evanng07 <ngoeva072@gmail.com>
2023-05-16 03:35:31 -0500

    used map instead

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,137 +1,151 @@
 import Card from "../components/UI/Card";
 import "../index.css";
 
+const player = [
+  {
+    name: "Patrick Mahomes",
+    team: "Kansas City Chiefs",
+    position: "QB",
+    number: 15,
+    fantasy_score: 23.4,
+  },
+  {
+    name: "Christian McCaffrey",
+    team: "San Fransisco 49ers",
+    position: "RB",
+    number: 23,
+    fantasy_score: 21.4,
+  },
+  {
+    name: "D'andre Swift",
+    team: "Philadelphia Eagles",
+    position: "RB",
+    number: 0,
+    fantasy_score: 16.2,
+  },
+  {
+    name: "D.J. Moore",
+    team: "Carolina Panthers",
+    position: "WR",
+    number: 2,
+    fantasy_score: 15.7,
+  },
+  {
+    name: "Jaylen Waddle",
+    team: "Miami Dolphins",
+    position: "WR",
+    number: 17,
+    fantasy_score: 17.6,
+  },
+  {
+    name: "Mark Andrews",
+    team: "Baltimore Ravens",
+    position: "TE",
+    number: 89,
+    fantasy_score: 18.1,
+  },
+  {
+    name: "Ceedee Lamb",
+    team: "Dallas Cowboys",
+    position: "WR",
+    number: 88,
+    fantasy_score: 19.4,
+  },
+  {
+    name: "Juju Smith-Schuster",
+    team: "New England Patriots",
+    position: "WR",
+    number: 9,
+    fantasy_score: 15.9,
+  },
+  {
+    name: "Adam Thielen",
+    team: "Carolina Panthers",
+    position: "WR",
+    number: 19,
+    fantasy_score: 13.4,
+  },
+  {
+    name: "Travis Kelce",
+    team: "Kansas City Chiefs",
+    position: "TE",
+    number: 87,
+    fantasy_score: 20.4,
+  },
+  {
+    name: "Darren Waller",
+    team: "New York Giants",
+    position: "TE",
+    number: 83,
+    fantasy_score: 15.4,
+  },
+  {
+    name: "Dak Prescott",
+    team: "Dallas Cowboys",
+    position: "QB",
+    number: 4,
+    fantasy_score: 18.4,
+  },
+  {
+    name: "Justin Jefferson",
+    team: "Minnesota Vikings",
+    position: "WR",
+    number: 18,
+    fantasy_score: 19.4,
+  },
+  {
+    name: "Aaron Rodgers",
+    team: "New York Jets",
+    position: "QB",
+    number: 12,
+    fantasy_score: 18.4,
+  },
+  {
+    name: "Saquon Barkley",
+    team: "New York Giants",
+    position: "RB",
+    number: 26,
+    fantasy_score: 18.4,
+  },
+  {
+    name: "Tyreek Hill",
+    team: "Miami Dolphins",
+    position: "WR",
+    number: 10,
+    fantasy_score: 18.4,
+  },
+  {
+    name: "Mike Williams",
+    team: "Los Angeles Chargers",
+    position: "WR",
+    number: 81,
+    fantasy_score: 15.4,
+  },
+  {
+    name: "Tony Pollard",
+    team: "Dallas Cowboys",
+    position: "RB",
+    number: 20,
+    fantasy_score: 18.4,
+  },    
+];
+
 const TeamPage = () => {
   return (
     <div className="justify-center items-center ml-32 mt-6">
       <div className="text-3xl font-bold text-white">Your Team</div>
       <div className="team">
-        <Card>
-          <div>Patrick Mahomes</div>
-          <div className="text-center mt-2">QB | 15</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Kansas City Chiefs
-          </div>
-        </Card>
-        <Card>
-          <div>Christian McCaffrey</div>
-          <div className="text-center mt-2">RB | 23</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            San Francisco 49ers
-          </div>
-        </Card>
-        <Card>
-          <div>D'eandre Swift</div>
-          <div className="text-center mt-2">RB | 0</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Philadelphia Eagles
-          </div>
-        </Card>
-        <Card>
-          <div>D.J. Moore</div>
-          <div className="text-center mt-2">WR | 2</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Carolina Panthers
-          </div>
-        </Card>
-        <Card>
-          <div>Jaylen Waddle</div>
-          <div className="text-center mt-2">WR | 17</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Miami Dolphins
-          </div>
-        </Card>
-        <Card>
-          <div>Mark Andrews</div>
-          <div className="text-center mt-2">TE | 89</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Baltimore Ravens
-          </div>
-        </Card>
-        <Card>
-          <div>Ceedee Lamb</div>
-          <div className="text-center mt-2">WR | 88</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Dallas Cowboys
-          </div>
-        </Card>
-        <Card>
-          <div>Juju Smith-Schuster</div>
-          <div className="text-center mt-2">WR | 9</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            New England Patriots
-          </div>
-        </Card>
-        <Card>
-          <div>Matt Prater</div>
-          <div className="text-center mt-2">K | 5</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Arizona Cardinals
-          </div>
-        </Card>
-        <Card>
-          <div>Adam Thielen</div>
-          <div className="text-center mt-2">WR | 19</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Carolina Panthers
-          </div>
-        </Card>
-        <Card>
-          <div>AJ Dillon</div>
-          <div className="text-center mt-2">RB | 28</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Green Bay Packers
-          </div>
-        </Card>
-        <Card>
-          <div>Rhamondre Stevenson</div>
-          <div className="text-center mt-2">RB | 38</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            New England Patriots
-          </div>
-        </Card>
-        <Card>
-          <div>Rashad Penny</div>
-          <div className="text-center mt-2">RB | 23</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Philadelphia Eagles
-          </div>
-        </Card>
-        <Card>
-          <div>Kenny Golladay</div>
-          <div className="text-center mt-2">WR | 19</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            New York Giants
-          </div>
-        </Card>
-        <Card>
-          <div>Michael Gallup</div>
-          <div className="text-center mt-2">WR | 13</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Dallas Cowboys
-          </div>
-        </Card>
-        <Card>
-          <div>Joe Mixon</div>
-          <div className="text-center mt-2">RB | 28</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Cincinnati Bengals
-          </div>
-        </Card>
-        <Card>
-          <div>Travis Kelce</div>
-          <div className="text-center mt-2">TE | 87</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Kansas City Chiefs
-          </div>
-        </Card>
-        <Card>
-          <div>Matt Gay</div>
-          <div className="text-center mt-2">K | 8</div>
-          <div className="text-center bottom-6 absolute text-xs left-0 right-0">
-            Indianapolis Colts
-          </div>
-        </Card>
+        {player.map((player) => (
+          <Card>
+            <div>{player.name}</div>
+            <div className="text-center mt-2">
+              {player.position} | {player.number}
+            </div>
+            <div className="text-center bottom-6 absolute text-xs left-0 right-0">
+              {player.team}
+            </div>
+          </Card>
+        ))}
       </div>
     </div>
   );
