8ff8ff7441704b1ab663cc7af2c38573854bcfb4
IbrahimKhanGH <iboo11khan@gmail.com>
2023-05-16 05:18:58 -0500

    josh allen image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,22 @@
 import Card from "../components/UI/Card";
 import "../index.css";
+import joshallen from "../utils/images/joshallen.png";
 
 const HomePage = () => {
   return (
-    
-      <div className="justify-center items-center ml-32 mt-6">
-        <div className="text-3xl font-bold text-white">
-          Welcome to the Fantasy Football Optimizer!
-        </div>
-      </div>
-    
+    <div className="justify-center items-center ml-32 mt-40">
+      <form className="relative flex">
+        <img
+          src={joshallen}
+          className="w-40 absolute left-0 top-0 -mt-10 ml-56"
+          alt="image"
+        />     
+        <input
+          className="rounded-3xl ml-64 w-1/2 mt-40 h-10 pl-4 z-10"
+          placeholder="Search for a player"
+        />
+      </form>
+    </div>
   );
 };
 
