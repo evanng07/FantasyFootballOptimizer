0de1b6bf4b9e4bf1b06ab1001db4cc67461c1347
IbrahimKhanGH <iboo11khan@gmail.com>
2023-05-16 07:13:22 -0500

    tooooooo sexy

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,5 +1,9 @@
 import classes from "./NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import GroupsIcon from "@mui/icons-material/Groups";
+import BarChartIcon from '@mui/icons-material/BarChart';
+import SearchIcon from "@mui/icons-material/Search";
 
 const NavBar = () => {
   return (
@@ -7,16 +11,24 @@ const NavBar = () => {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <NavLink to="/home"  className={classes.text}>Home</NavLink>
+            <NavLink to="/home" className={classes.text}>
+              <HomeIcon className={classes.icon} fontSize="large" />
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/team" className={classes.text}>Team</NavLink>
+            <NavLink to="/team" className={classes.text}>
+              <GroupsIcon className={classes.icon} fontSize="large" />
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/stat" className={classes.text}>Stats</NavLink>
+            <NavLink to="/stat" className={classes.text}>
+              <BarChartIcon className={classes.icon} fontSize="large" />
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/search" className={classes.text}>Search</NavLink>
+            <NavLink to="/search" className={classes.text}>
+              <SearchIcon className={classes.icon} fontSize="large" />
+            </NavLink>
           </li>
         </ul>
       </nav>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,11 +12,27 @@ const HomePage = () => {
             className="w-40 absolute left-0 top-0 -mt-10 ml-72"
             alt="image"
           />
-          <h2 className="text-white w-100 font-bold text-5xl absolute" style={{marginLeft: "485px", marginTop: "60px"}}>Fantasy Optimizer</h2>
+          <h2
+            className="text-white w-100 font-bold text-5xl absolute"
+            style={{ marginLeft: "485px", marginTop: "60px" }}
+          >
+            Fantasy Optimizer
+          </h2>
+          <h2
+            className="w-100 absolute"
+            style={{
+              marginLeft: "580px",
+              marginTop: "115px",
+              color: "#D3D3D3",
+              fontSize: "14px",
+            }}
+          >
+            Fantasy Football Simplified
+          </h2>
         </div>
         <input
           className=" absolute rounded-3xl w-1/2 mt-40 h-10 pl-4 z-10"
-          style={{transform: "translate(48%, -10%)"}}
+          style={{ transform: "translate(48%, -10%)" }}
           placeholder="Search for a player"
         />
       </form>
