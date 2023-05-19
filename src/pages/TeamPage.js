efd926a6fd058c6dc71430328bebfabeaba48cdc
evanng07 <ngoeva072@gmail.com>
2023-05-19 01:00:27 -0500

    changed some formatting

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -3,8 +3,8 @@ import "../index.css";
 
 const TeamPage = () => {
   return (
-    <div className="justify-center items-center ml-32 mt-6">
-      <div className="text-3xl font-bold text-white">Your Team</div>
+    <div className="ml-40 mt-6">
+      <div className="text-3xl font-bold text-white" style={{marginLeft: "532px"}}>Your Team</div>
       <div className="team">
         {player.map((player) => (
           <Card>
